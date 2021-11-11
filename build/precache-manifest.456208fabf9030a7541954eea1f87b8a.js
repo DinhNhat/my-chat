72cb355c6af563a103c1b9ee857e3fa06ae6c934
DinhNhat <nguyendinhnhat206@gmail.com>
2021-11-11 16:13:52 -0500

    first commit

---
+++ build/precache-manifest.456208fabf9030a7541954eea1f87b8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7315f36979e7d57f6baf94d92bdff2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75070b662d1566b66cc8",
+    "url": "./static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "5116e15d2112729e1678",
+    "url": "./static/js/2.e8674f43.chunk.js"
+  },
+  {
+    "revision": "75070b662d1566b66cc8",
+    "url": "./static/js/main.92914553.chunk.js"
+  },
+  {
+    "revision": "aa4ef238f3ddd9b5539c",
+    "url": "./static/js/runtime~main.c94723ea.js"
+  }
+]);
